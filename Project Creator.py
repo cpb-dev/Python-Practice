18d56058b15a27d34818c494efa29ec13b65fc73
Conor Bailey <cpbailey025@gmail.com>
2021-07-03 19:20:56 +0100

    Additions made to the project creator app
    Expanding on the current concept, however it works this time. The application creates the project in the desired location and opens it in the desired IDE.

--- Project Creator.py
+++ Project Creator.py
@@ -1,31 +1,41 @@
 import os
 import sys
+import subprocess
 
 #Application intro
 print("Hello and Welcome to the Project Builder!!!")
 print("-------------------------------------------")
 print("How may I be of assistance?")
-print("To view project types type "?"")
+print("To view project types type (?) ")
 
-type = input("Enter the project type:")
+type = input("Enter the project type: ")
 
-if type == "?"
-    print("Types include Python and Web")
-    type = input("Enter the project type:")
-else
-    name = input("What is the file name?: ")
+#Gathering inputs to create appropriate files
+if type == "?":
+    print("Choose between Python or HTML")
+    type = input("Enter the project type: ")
 
-    if type == "Python"
-        save_path = "C:\Users\cpbai\PlayGrounds\Python"
-        extension = ".py"
-        elif type == "Web"
-            save_path = "C:\Users\cpbai\PlayGrounds\Web"
-            extension = ".html"
-
-file = name + extension
-completed_name = os.path.join(save_path, file)
-
-f = open(completed_name, "x")
-f.write("Created the file in " + completed_name)
+#Seperate statement to the previous to allow the new type to be collected
+if type == "Python":
+    name = input("Enter the files desired name: ")
+    extension = ".py"
+    proj = name + extension
+    f = open(proj, "x")
+    app = 'C:\\Users\\cpbai\\AppData\\Local\\atom\\atom.exe' #Location of IDE
+    subprocess.call([app, proj])
+    exit()
+#The if statement for html projects
+elif type == "HTML" or type == "html":
+    name = input("Enter the files desired name: ")
+    extension = ".html"
+    location = "C:\\Users\\cpbai\\PlayGrounds\\Web\\" + name
+    os.mkdir(location)
+    proj = location + "\\index" + extension
+    f = open(proj, "x")
+    app = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\devenv.exe"
+    subprocess.call([app, proj])
+    exit()
+else:
+    print("Please restart the program and type a valid input")
 
 input("Press ENTER to Close!!!")
